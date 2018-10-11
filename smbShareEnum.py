e473d639e9f20810aa5e1274c159b5faff3e13b2
Gunnarbiym <42415985+Gunnarbiym@users.noreply.github.com>
2018-10-11 00:00:31 -0400

    Add files via upload
    Updated formatting to make the code look nicer and added comments and documentation

--- smbShareEnum.py
+++ smbShareEnum.py
@@ -1,35 +1,45 @@
+############################################################
+#       Author: Gunnar Sorensen                            #
+#       Contact: gs6601@rit.edu                            #
+#       Description: SMB share enumerator which takes in   #
+#       a username, password, file, and domain name and    #
+#       gives the user information the shares on smb and   #
+#       what files those shares contain.                   #
+############################################################
+
+
 import sys
 import argparse
 from smb.SMBConnection import SMBConnection
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Process SMB information.')
-    parser.add_argument("-u", "--username")
-    parser.add_argument("-p", "--password")
-    parser.add_argument("-f", "--file", dest="filename")
-    parser.add_argument("-d", "--domain")
+    parser.add_argument("-u", "--username")                 #argument to provide username
+    parser.add_argument("-p", "--password")                 #argument to provide password
+    parser.add_argument("-f", "--file", dest="filename")    #argument to provide file with a list of all systems you want to enumerate
+    parser.add_argument("-d", "--domain")                   #argument to provide domain containing smb
 
     args = parser.parse_args()
 
-    with open(args.filename) as f:
-        for system_name in f:
-            print('### Analyzing system: ' + system_name)
+###################################################################
+# Opens a file that contains a list of system names to go through #
+# and find all shares and the files with in those shares.         #
+###################################################################
+
+    with open(args.filename) as f:                          # Open file with system names
+        for system_name in f:                               # Loop through file
+            print('Enumerating over system: ' + system_name)
             conn = SMBConnection(args.username, args.password, 'enumerator', system_name, args.domain,
-            use_ntlm_v2=True, sign_options=SMBConnection.SIGN_WHEN_SUPPORTED, is_direct_tcp=True)
-
-            #Attempt to connect
-            conn.connect(system_name,445)
-
-
-            Response = conn.listShares(timeout=30)  # Set response variable that contains list of shares
+            use_ntlm_v2=True, sign_options=1, is_direct_tcp=False)
+            conn.connect(system_name, 139) # Attempt to connect to the system
+            Shares = conn.listShares(timeout=30)  # Set Shares variable that contains list of shares
             print('Shares for: ' + system_name)
-            for i in range(len(Response)):  # iterate through the list of shares
-                print("  Share: ",i," =", Response[i].name)
-                Files = conn.listPath(Response[i].name,'/',timeout=30) #Get a list of files in the share
-                print('    Files for: ' + system_name + '/' + "  Share: ",i," =",Response[i].name)
+            for i in range(len(Shares)):  # iterate through the list of shares
+                print("Share: ",i," =", Shares[i].name)
+                Files = conn.listPath(Shares[i].name,'/',timeout=30) # Get a list of files in the share
+                print('Files for: ' + system_name + '/' + "  Share: ",i," =",Shares[i].name)
                 for i in range(len(Files)):
-                    print("    File[",i,"] =", Files[i].filename)
-
+                    print("File[",i,"] =", Files[i].filename)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
