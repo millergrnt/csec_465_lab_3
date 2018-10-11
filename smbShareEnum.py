d6a132328f29ea8bc8d180b4d1a34ef8322c3d0c
fg8643 <fg8643@rit.edu>
2018-10-11 13:45:55 -0400

    final commit for the SMB

--- smbShareEnum.py
+++ smbShareEnum.py
@@ -26,20 +26,26 @@ def main(argv):
 # and find all shares and the files with in those shares.         #
 ###################################################################
 
-    with open(args.filename) as f:                          # Open file with system names
-        for system_name in f:                               # Loop through file
+    with open(args.filename) as f:
+
+        # Open file with system names
+        for system_name in f:
+            # Loop through file
             print('Enumerating over system: ' + system_name)
             conn = SMBConnection(args.username, args.password, 'enumerator', system_name, args.domain,
-            use_ntlm_v2=True, sign_options=1, is_direct_tcp=False)
+            use_ntlm_v2=True, sign_options=SMBConnection.SIGN_WHEN_SUPPORTED, is_direct_tcp=False)
             conn.connect(system_name, 139) # Attempt to connect to the system
             Shares = conn.listShares(timeout=30)  # Set Shares variable that contains list of shares
             print('Shares for: ' + system_name)
             for i in range(len(Shares)):  # iterate through the list of shares
+                if "$" in Shares[i].name:
+                    continue
                 print("Share: ",i," =", Shares[i].name)
                 Files = conn.listPath(Shares[i].name,'/',timeout=30) # Get a list of files in the share
                 print('Files for: ' + system_name + '/' + "  Share: ",i," =",Shares[i].name)
                 for i in range(len(Files)):
                     print("File[",i,"] =", Files[i].filename)
+            conn.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
